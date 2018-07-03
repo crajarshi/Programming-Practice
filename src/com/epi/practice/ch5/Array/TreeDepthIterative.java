2116d6a046ba8044dbdc85da3473da205cde2c30
Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>
2018-07-02 19:07:28 -0700

    FACEBOOK LC Problems
    Signed-off-by: Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>

--- src/com/epi/practice/ch5/Array/TreeDepthIterative.java
+++ src/com/epi/practice/ch5/Array/TreeDepthIterative.java
@@ -60,9 +60,9 @@ public class TreeDepthIterative {
     }
 
     public class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
 
         TreeNode(int x) {
             val = x;
---
+++ src/leetcode/TreeNode.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+/**
+ * Created by Raj on 7/2/18.
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
