f2fd5931d4a9b9c682b1fb060908a9d2a862cfc3
Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>
2018-07-05 21:27:57 -0700

    Facebook LC Problems
    Signed-off-by: Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>

--- src/leetcode/RemoveInvalidParenthesis.java
+++ src/leetcode/RemoveInvalidParenthesis.java
@@ -18,7 +18,8 @@ import java.util.*;
 public class RemoveInvalidParenthesis {
 
     /**
-     * The idea is straightforward, with the input string s, we generate all possible states by removing one ( or ), check if they are valid, if found valid ones on the current level, put them to the final result list and we are done, otherwise, add them to a queue and carry on to the next level.
+     * The idea is straightforward, with the input string s, we generate all possible states by removing one ( or ), check if they are valid,
+     * if found valid ones on the current level, put them to the final result list and we are done, otherwise, add them to a queue and carry on to the next level.
      * <p>
      * The good thing of using BFS is that we can guarantee the number of parentheses that need to be removed is minimal, also no recursion call is needed in BFS.
      * <p>
@@ -26,9 +27,14 @@ public class RemoveInvalidParenthesis {
      * <p>
      * Time complexity:
      * <p>
-     * In BFS we handle the states level by level, in the worst case, we need to handle all the levels, we can analyze the time complexity level by level and add them up to get the final complexity.
+     * In BFS we handle the states level by level, in the worst case, we need to handle all the levels,
+     * we can analyze the time complexity level by level and add them up to get the final complexity.
      * <p>
-     * On the first level, there's only one string which is the input string s, let's say the length of it is n, to check whether it's valid, we need O(n) time. On the second level, we remove one ( or ) from the first level, so there are C(n, n-1) new strings, each of them has n-1 characters, and for each string, we need to check whether it's valid or not, thus the total time complexity on this level is (n-1) x C(n, n-1). Come to the third level, total time complexity is (n-2) x C(n, n-2), so on and so forth...
+     * On the first level, there's only one string which is the input string s, let's say the length of it is n,
+     * to check whether it's valid, we need O(n) time. On the second level, we remove one ( or ) from the first level,
+     * so there are C(n, n-1) new strings, each of them has n-1 characters, and for each string,
+     * we need to check whether it's valid or not, thus the total time complexity on this level is (n-1) x C(n, n-1).
+     * Come to the third level, total time complexity is (n-2) x C(n, n-2), so on and so forth...
      * <p>
      * Finally we have this formula:
      * <p>
