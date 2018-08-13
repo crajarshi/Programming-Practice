14b06f0b75bd8fe6c76e94dc9a20c65ed91c6cf5
Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>
2018-08-12 17:09:08 -0700

    Google
    Signed-off-by: Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>

---
+++ src/leetcode/MergeNewInterval.java
@@ -0,0 +1,67 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
+ * <p>
+ * You may assume that the intervals were initially sorted according to their start times.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+ * Output: [[1,5],[6,9]]
+ * Example 2:
+ * <p>
+ * Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+ * Output: [[1,2],[3,10],[12,16]]
+ * Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+ */
+public class MergeNewInterval {
+
+    public static List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+        List<Interval> result = new LinkedList<>();
+        int i = 0;
+        // add all the intervals ending before newInterval starts
+        while (i < intervals.size() && intervals.get(i).end < newInterval.start)
+            result.add(intervals.get(i++));
+        // merge all overlapping intervals to one considering newInterval
+        while (i < intervals.size() && intervals.get(i).start <= newInterval.end) {
+            newInterval = new Interval( // we could mutate newInterval here also
+                    Math.min(newInterval.start, intervals.get(i).start),
+                    Math.max(newInterval.end, intervals.get(i).end));
+            i++;
+        }
+        result.add(newInterval); // add the union of intervals we got
+        // add all the rest
+        while (i < intervals.size()) result.add(intervals.get(i++));
+        return result;
+    }
+
+    public static void main(String[] args) {
+        List<Interval> lstIntervals = new ArrayList<>();
+        lstIntervals.add(new Interval(1, 2));
+        lstIntervals.add(new Interval(3, 5));
+        lstIntervals.add(new Interval(6, 7));
+        lstIntervals.add(new Interval(8, 10));
+        lstIntervals.add(new Interval(12, 16));
+        System.out.println(insert(lstIntervals, new Interval(4, 8)));
+    }
+
+    public static class Interval {
+        int start;
+        int end;
+
+        Interval() {
+            start = 0;
+            end = 0;
+        }
+
+        Interval(int s, int e) {
+            start = s;
+            end = e;
+        }
+    }
+}
