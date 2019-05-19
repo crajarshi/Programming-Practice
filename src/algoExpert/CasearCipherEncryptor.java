a439f7c2249ede674b53a4b618f204a5d9734f89
Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>
2019-05-18 19:15:39 -0700

    Algoexpert initial commit.
    Signed-off-by: Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>

---
+++ src/algoExpert/CasearCipherEncryptor.java
@@ -0,0 +1,24 @@
+package algoExpert;
+
+/**
+ * Created by Raj on 5/18/19.
+ */
+public class CasearCipherEncryptor {
+
+    // O(n) time | O(n) space
+    public static String caesarCypherEncryptor(String str, int key) {
+        char[] newLetters = new char[str.length()];
+        int newKey = key % 26;
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        for (int i = 0; i < str.length(); i++) {
+            newLetters[i] = getNewLetter(str.charAt(i), newKey, alphabet);
+        }
+        return new String(newLetters);
+    }
+
+    public static char getNewLetter(char letter, int key, String alphabet) {
+        int newLetterCode = alphabet.indexOf(letter) + key;
+        return newLetterCode <= 25 ? alphabet.charAt(newLetterCode) : alphabet.charAt(-1 + newLetterCode % 25);
+    }
+
+}
---
+++ src/algoExpert/FindClosestInBST.java
@@ -0,0 +1,59 @@
+package algoExpert;
+
+/**
+ * Created by Raj on 5/18/19.
+ */
+public class FindClosestInBST {
+    // Average: O(log(n)) time | O(log(n)) space
+    // Worst: O(n) time | O(n) space
+    public static int findClosestValueInBst(BST tree, int target) {
+        return findClosestValueInBst(tree, target, Double.MAX_VALUE);
+    }
+
+    public static int findClosestValueInBst(BST tree, int target, double closest) {
+        if (Math.abs(target - closest) > Math.abs(target - tree.value)) {
+            closest = tree.value;
+        }
+        if (target < tree.value && tree.left != null) {
+            return findClosestValueInBst(tree.left, target, closest);
+        } else if (target > tree.value && tree.right != null) {
+            return findClosestValueInBst(tree.right, target, closest);
+        } else {
+            return (int) closest;
+        }
+    }
+
+    // Average: O(log(n)) time | O(1) space
+    // Worst: O(n) time | O(1) space
+    public static int findClosestValueInBstIterative(BST tree, int target) {
+        return findClosestValueInBstIterative(tree, target, Double.MAX_VALUE);
+    }
+
+    public static int findClosestValueInBstIterative(BST tree, int target, double closest) {
+        BST currentNode = tree;
+        while (currentNode != null) {
+            if (Math.abs(target - closest) > Math.abs(target - currentNode.value)) {
+                closest = currentNode.value;
+            }
+            if (target < currentNode.value) {
+                currentNode = currentNode.left;
+            } else if (target > currentNode.value) {
+                currentNode = currentNode.right;
+            } else {
+                break;
+            }
+        }
+        return (int) closest;
+    }
+
+
+    static class BST {
+        public int value;
+        public BST left;
+        public BST right;
+
+        public BST(int value) {
+            this.value = value;
+        }
+    }
+}
