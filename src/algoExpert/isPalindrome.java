950fb23efe8a9bf8f0998e2f3a36ed0153149c41
Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>
2019-05-18 18:37:36 -0700

    Algoexpert initial commit.
    Signed-off-by: Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>

---
+++ src/algoExpert/isPalindrome.java
@@ -0,0 +1,18 @@
+package algoExpert;
+
+/**
+ * Created by Raj on 5/18/19.
+ */
+public class isPalindrome {
+
+    public static boolean isPalindrome(String str) {
+        // Write your code here.
+        int leftIdx = 0, rightIdx = str.length() - 1;
+        while (leftIdx < rightIdx) {
+            if (str.charAt(leftIdx++) != str.charAt(rightIdx--))
+                return false;
+        }
+
+        return true;
+    }
+}
