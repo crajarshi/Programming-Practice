73a5b8248f49e0ea35b734e558b97c4bda737df5
Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>
2019-05-18 23:40:00 -0700

    Algoexpert.
    Signed-off-by: Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>

--- src/algoExpert/LongestPalindromicSubstring.java
+++ src/algoExpert/LongestPalindromicSubstring.java
@@ -18,6 +18,19 @@ public class LongestPalindromicSubstring {
         return longest;
     }
 
+    public static boolean isPalindrome(String str) {
+        int leftIdx = 0;
+        int rightIdx = str.length() - 1;
+        while (leftIdx < rightIdx) {
+            if (str.charAt(leftIdx) != str.charAt(rightIdx)) {
+                return false;
+            }
+            leftIdx++;
+            rightIdx--;
+        }
+        return true;
+    }
+
     // O(n^2) time | O(1) space
     public static String longestPalindromicSubstringOptimized(String str) {
         int[] currentLongest = {0, 1};
