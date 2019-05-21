2b4c1045ec2fc2ba957e0466bec9a914040abe40
Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>
2019-05-20 22:19:33 -0700

    Algoexpert with questions.
    Signed-off-by: Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>

---
+++ src/Linkedin/TwoSumIIIDatastructuredesign.java
@@ -0,0 +1,81 @@
+package Linkedin;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Design and implement a TwoSum class. It should support the following operations: add and find.
+ * <p>
+ * add - Add the number to an internal data structure.
+ * find - Find if there exists any pair of numbers which sum is equal to the value.
+ * <p>
+ * Example 1:
+ * <p>
+ * add(1); add(3); add(5);
+ * find(4) -> true
+ * find(7) -> false
+ * Example 2:
+ * <p>
+ * add(3); add(1); add(2);
+ * find(3) -> true
+ * find(6) -> false
+ */
+public class TwoSumIIIDatastructuredesign {
+
+    /*
+    If consider more find and less add or we only care time complexity in finding.For example,
+    add operation can be pre-done.
+
+
+     */
+    Set<Integer> sums = new HashSet<>();
+    Set<Integer> nums = new HashSet<>();
+
+    /**
+     * Initialize your data structure here.
+     */
+    public TwoSumIIIDatastructuredesign() {
+
+    }
+
+    /**
+     * Add the number to an internal data structure..
+     */
+    public void add(int number) {
+        for (int n : nums) sums.add(number + n);
+        nums.add(number);
+    }
+
+    /**
+     * Find if there exists any pair of numbers which sum is equal to the value.
+     */
+    public boolean find(int value) {
+        return sums.contains(value);
+    }
+
+    /*
+    Map<Integer, Integer> map = new HashMap<>();
+
+     Initialize your data structure here.
+    public TwoSum() {
+
+    }
+
+    /** Add the number to an internal data structure..
+    public void add(int number) {
+        map.put(number, map.getOrDefault(number, 0) + 1);
+    }
+
+    /** Find if there exists any pair of numbers which sum is equal to the value.
+    public boolean find(int value) {
+        for(int key : map.keySet()) {
+            int remain = value - key;
+            if (remain == key && map.get(key) >= 2) return true;
+            if (remain != key && map.containsKey(remain)) return true;
+        }
+        return false;
+    }
+     */
+
+
+}
