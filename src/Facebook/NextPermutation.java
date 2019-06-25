3adf06075f4ee040e1f5f80df4935326802aea59
Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>
2019-06-24 22:00:42 -0700

    Facebook problems added
    Signed-off-by: Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>

---
+++ src/Facebook/NextPermutation.java
@@ -0,0 +1,48 @@
+package Facebook;
+
+/**
+ * Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+ * <p>
+ * If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
+ * <p>
+ * The replacement must be in-place and use only constant extra memory.
+ * <p>
+ * Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
+ * <p>
+ * 1,2,3 → 1,3,2
+ * 3,2,1 → 1,2,3
+ * 1,1,5 → 1,5,1
+ */
+public class NextPermutation {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i + 1] <= nums[i]) {//Find first decreasing
+            // element scanning from right.
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {// find first element
+                // greater than the decreasing element and swap it.
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    private void reverse(int[] nums, int start) {
+        int i = start, j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
---
+++ src/Facebook/RemoveInvalidParentheses.java
@@ -0,0 +1,22 @@
+package Facebook;
+
+/**
+ * Remove the minimum number of invalid parentheses in order to make the input string valid. Return all possible results.
+ * <p>
+ * Note: The input string may contain letters other than the parentheses ( and ).
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: "()())()"
+ * Output: ["()()()", "(())()"]
+ * Example 2:
+ * <p>
+ * Input: "(a)())()"
+ * Output: ["(a)()()", "(a())()"]
+ * Example 3:
+ * <p>
+ * Input: ")("
+ * Output: [""]
+ */
+public class RemoveInvalidParentheses {
+}
