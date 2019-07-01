f2b8a7f139d6af81331abc5c809580940e878c8e
Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>
2019-06-30 21:47:06 -0700

    Facebook problems added
    Signed-off-by: Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>

--- src/Facebook/IntervalListIntersection.java
+++ src/Facebook/IntervalListIntersection.java
@@ -2,6 +2,7 @@ package Facebook;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.PriorityQueue;
 
 /**
  * Given two lists of closed intervals, each list of intervals is pairwise disjoint and in sorted order.
@@ -38,4 +39,78 @@ public class IntervalListIntersection {
 
         return res.toArray(new int[res.size()][2]);
     }
+
+    public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {//[[[1,2],[5,6]],[[1,3]],[[4,10]]]
+        List<Interval> result = new ArrayList<>();
+/**
+ The idea is to just add all the intervals to the priority queue.
+ (NOTE that it is not matter how many different people are there for the algorithm. becuase we just need to find a gap in the time line.
+
+ priority queue - sorted by start time, and for same start time sort by either largest end time or smallest (it is not matter).
+ Everytime you poll from priority queue, just make sure it doesn't intersect with previous interval.
+ This mean that there is no common interval. Everyone is free time.
+ */
+        PriorityQueue<Interval> pq = new PriorityQueue<>((a, b) -> a.start - b.start);
+        schedule.forEach(e -> pq.addAll(e));//[[[1,2],[1,3]],[[4,10]],[[5,6]]]
+
+        Interval temp = pq.poll();
+        while (!pq.isEmpty()) {
+            if (temp.end < pq.peek().start) { // no intersect
+                result.add(new Interval(temp.end, pq.peek().start));//[3,4]
+                temp = pq.poll(); // becomes the next temp interval
+            } else { // intersect or sub merged
+                temp = temp.end < pq.peek().end ? pq.peek() : temp;
+                pq.poll();
+            }
+        }
+        return result;
+    }
+
+    /**
+     * You are given a list schedule of employees, which represents the
+     * working time for each employee.
+     * <p>
+     * Each employee has a list of non-overlapping Intervals, and these intervals are in sorted order.
+     * <p>
+     * Return the list of finite intervals representing common, positive-length free time for all employees, also in sorted order.
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: schedule = [[[1,2],[5,6]],[[1,3]],[[4,10]]]
+     * Output: [[3,4]]
+     * Explanation:
+     * There are a total of three employees, and all common
+     * free time intervals would be [-inf, 1], [3, 4], [10, inf].
+     * We discard any intervals that contain inf as they aren't finite.
+     * <p>
+     * <p>
+     * Example 2:
+     * <p>
+     * Input: schedule = [[[1,3],[6,7]],[[2,4]],[[2,5],[9,12]]]
+     * Output: [[5,6],[7,9]]
+     * <p>
+     * <p>
+     * (Even though we are representing Intervals in the form [x, y], the objects inside are Intervals,
+     * not lists or arrays. For example, schedule[0][0].start = 1, schedule[0][0].end = 2, and schedule[0][0][0] is not defined.)
+     * <p>
+     * Also, we wouldn't include intervals like [5, 5] in our answer, as they have zero length.
+     * <p>
+     * <p>
+     * Definition for an interval.
+     **/
+    public class Interval {
+        int start;
+        int end;
+
+        Interval() {
+            start = 0;
+            end = 0;
+        }
+
+        Interval(int s, int e) {
+            start = s;
+            end = e;
+        }
+    }
 }
+
