ba4e9c2f40d43c64782e3aa36bd023261efad30a
Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>
2019-07-03 22:16:25 -0700

    Facebook problems added
    Signed-off-by: Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>

---
+++ src/Facebook/BestTimetobuyandsellstock.java
@@ -0,0 +1,146 @@
+package Facebook;
+
+/**
+ * Say you have an array for which the ith element is the price of a given stock on day i.
+ * <p>
+ * Design an algorithm to find the maximum profit. You may complete at most two transactions.
+ * <p>
+ * Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: [3,3,5,0,0,3,1,4]
+ * Output: 6
+ * Explanation: Buy on day 4 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+ * Then buy on day 7 (price = 1) and sell on day 8 (price = 4), profit = 4-1 = 3.
+ * Example 2:
+ * <p>
+ * Input: [1,2,3,4,5]
+ * Output: 4
+ * Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+ * Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are
+ * engaging multiple transactions at the same time. You must sell before buying again.
+ * Example 3:
+ * <p>
+ * Input: [7,6,4,3,1]
+ * Output: 0
+ * Explanation: In this case, no transaction is done, i.e. max profit = 0.
+ */
+public class BestTimetobuyandsellstock {
+    /**
+     * The thinking is simple and is inspired by the best solution from Single Number II (I read through the discussion after I use DP).
+     * Assume we only have 0 money at first;
+     * 4 Variables to maintain some interested 'ceilings' so far:
+     * The maximum of if we've just buy 1st stock, if we've just sold 1nd stock, if we've just buy 2nd stock, if we've just sold 2nd stock.
+     * Very simple code too and work well.
+     */
+    public static int maxProfit3(int[] prices) {
+        if (prices == null || prices.length <= 1) return 0;
+        int oneBuy = Integer.MIN_VALUE;
+        int oneBuyOneSell = 0;
+        int twoBuy = Integer.MIN_VALUE;
+        int twoBuyTwoSell = 0;
+        for (int p : prices) {
+            twoBuyTwoSell = Math.max(twoBuyTwoSell, p + twoBuy);
+            twoBuy = Math.max(twoBuy, oneBuyOneSell - p);
+            oneBuyOneSell = Math.max(oneBuyOneSell, p + oneBuy);
+            oneBuy = Math.max(oneBuy, -p);
+        }
+        return Math.max(oneBuyOneSell, twoBuyTwoSell);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 3, 5, 0, 0, 3, 1, 4};
+        System.out.println(maxProfit3(arr));
+    }
+
+    /**
+     * Say you have an array for which the ith element is the price of a given stock on day i.
+     * <p>
+     * Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+     * <p>
+     * Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+     */
+
+    public int maxProfit2(int[] prices) {
+        int i = 0;
+        int valley = prices[0];
+        int peak = prices[0];
+        int maxprofit = 0;
+        while (i < prices.length - 1) {
+            while (i < prices.length - 1 && prices[i] >= prices[i + 1])
+                i++;
+            valley = prices[i];
+            while (i < prices.length - 1 && prices[i] <= prices[i + 1])
+                i++;
+            peak = prices[i];
+            maxprofit += peak - valley;
+        }
+        return maxprofit;
+    }
+
+    /**
+     * Say you have an array for which the ith element is the price of a given stock on day i.
+     * <p>
+     * Design an algorithm to find the maximum profit. You may complete at most k transactions.
+     * <p>
+     * Note:
+     * You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: [2,4,1], k = 2
+     * Output: 2
+     * Explanation: Buy on day 1 (price = 2) and sell on day 2 (price = 4), profit = 4-2 = 2.
+     * Example 2:
+     * <p>
+     * Input: [3,2,6,5,0,3], k = 2
+     * Output: 7
+     * Explanation: Buy on day 2 (price = 2) and sell on day 3 (price = 6), profit = 6-2 = 4.
+     * Then buy on day 5 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+     */
+
+    public int maxProfit2simple(int[] prices) {
+        int maxprofit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1])
+                maxprofit += prices[i] - prices[i - 1];
+        }
+        return maxprofit;
+    }
+
+    /**
+     * dp[i, j] represents the max profit up until prices[j] using at most i transactions.
+     * dp[i, j] = max(dp[i, j-1], prices[j] - prices[jj] + dp[i-1, jj]) { jj in range of [0, j-1] }
+     * = max(dp[i, j-1], prices[j] + max(dp[i-1, jj] - prices[jj]))
+     * dp[0, j] = 0; 0 transactions makes 0 profit
+     * dp[i, 0] = 0; if there is only one price data point you can't make any transaction.
+     */
+
+    public int maxProfit4(int k, int[] prices) {
+        int n = prices.length;
+        if (n <= 1)
+            return 0;
+
+        //if k >= n/2, then you can make maximum number of transactions.
+        if (k >= n / 2) {
+            int maxPro = 0;
+            for (int i = 1; i < n; i++) {
+                if (prices[i] > prices[i - 1])
+                    maxPro += prices[i] - prices[i - 1];
+            }
+            return maxPro;
+        }
+
+        int[][] dp = new int[k + 1][n];
+        for (int i = 1; i <= k; i++) {
+            int localMax = dp[i - 1][0] - prices[0];
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.max(dp[i][j - 1], prices[j] + localMax);
+                localMax = Math.max(localMax, dp[i - 1][j] - prices[j]);
+            }
+        }
+        return dp[k][n - 1];
+    }
+
+}
