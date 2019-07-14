28e79877666b1893c48a1a3fc6bd0310c84eab48
Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>
2019-07-13 20:05:15 -0700

    Facebook problems added
    Signed-off-by: Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>

---
+++ src/Facebook/Bipartitegraph.java
@@ -0,0 +1,71 @@
+package Facebook;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Created by Raj on 7/13/19.
+ */
+public class Bipartitegraph {
+    /**
+     * Our goal is trying to use two colors to color the graph and see if there are any adjacent nodes having the same color.
+     * Initialize a color[] array for each node. Here are three states for colors[] array:
+     * 0: Haven't been colored yet.
+     * 1: Blue.
+     * -1: Red.
+     * For each node ->
+     * <p>
+     * If it hasn't been colored, use a color to color it. Then use the other color to color all its adjacent nodes (DFS).
+     * If it has been colored, check if the current color is the same as the color that is going to be used to color it.
+     */
+    public boolean isBipartiteDFS(int[][] graph) {
+        int n = graph.length;
+        int[] colors = new int[n];
+
+        for (int i = 0; i < n; i++) {              //This graph might be a disconnected graph. So check each unvisited node.
+            if (colors[i] == 0 && !validColor(graph, colors, 1, i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean validColor(int[][] graph, int[] colors, int color, int node) {
+        if (colors[node] != 0) {
+            return colors[node] == color;
+        }
+        colors[node] = color;
+        for (int next : graph[node]) {
+            if (!validColor(graph, colors, -color, next)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isBipartiteBFS(int[][] graph) {
+        int len = graph.length;
+        int[] colors = new int[len];
+
+        for (int i = 0; i < len; i++) {
+            if (colors[i] != 0) continue;
+            Queue<Integer> queue = new LinkedList<>();
+            queue.offer(i);
+            colors[i] = 1;   // Blue: 1; Red: -1.
+
+            while (!queue.isEmpty()) {
+                int cur = queue.poll();
+                for (int next : graph[cur]) {
+                    if (colors[next] == 0) {          // If this node hasn't been colored;
+                        colors[next] = -colors[cur];  // Color it with a different color;
+                        queue.offer(next);
+                    } else if (colors[next] != -colors[cur]) {   // If it is colored and its color is different, return false;
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+}
