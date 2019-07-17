615f3bfdc1dd4795e42a2fe751c6d04e0d8844f6
Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>
2019-07-16 21:21:37 -0700

    Facebook problems added
    Signed-off-by: Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>

---
+++ src/Facebook/FindPeakElement.java
@@ -0,0 +1,44 @@
+package Facebook;
+
+/**
+ * A peak element is an element that is greater than its neighbors.
+ * <p>
+ * Given an input array nums, where nums[i] ≠ nums[i+1], find a peak element and return its index.
+ * <p>
+ * The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
+ * <p>
+ * You may imagine that nums[-1] = nums[n] = -∞.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [1,2,3,1]
+ * Output: 2
+ * Explanation: 3 is a peak element and your function should return the index number 2.
+ * Example 2:
+ * <p>
+ * Input: nums = [1,2,1,3,5,6,4]
+ * Output: 1 or 5
+ * Explanation: Your function can return either index number 1 where the peak element is 2,
+ * or index number 5 where the peak element is 6.
+ * Note:
+ * <p>
+ * Your solution should be in logarithmic complexity.
+ */
+public class FindPeakElement {
+    public static int findPeakElement(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] > nums[mid + 1])
+                r = mid;
+            else
+                l = mid + 1;
+        }
+        return l;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 1, 3, 5, 6, 4};
+        System.out.println(findPeakElement(nums));
+    }
+}
