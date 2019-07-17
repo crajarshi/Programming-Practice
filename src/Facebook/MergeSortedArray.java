4b3c6e5780076f71c2a10027ef71e1d492e99c9f
Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>
2019-07-16 21:14:22 -0700

    Facebook problems added
    Signed-off-by: Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>

--- src/Facebook/MergeSortedArray.java
+++ src/Facebook/MergeSortedArray.java
@@ -20,4 +20,17 @@ public class MergeSortedArray {
         // add missing elements from nums2
         System.arraycopy(nums2, 0, nums1, 0, p2 + 1);//arraycopy(Object src, int srcPos, Object dest, int destPos, int length)
     }
+
+    public void merge2(int[] nums1, int m, int[] nums2, int n) {
+
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int p = m + n - 1;
+
+        while (p1 >= 0 && p2 >= 0) {
+            nums1[p--] = (nums1[p1] < nums2[p2]) ? nums2[p2--] : nums1[p1--];
+        }
+        while (p2 >= 0)
+            nums1[p--] = nums2[p2--];
+    }
 }
