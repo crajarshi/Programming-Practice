d453fe272cb36a0c82b2f9bc963adf0dd555269a
Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>
2019-07-20 22:47:26 -0700

    Facebook problems added
    Signed-off-by: Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>

---
+++ src/Facebook/ValidPalindrome.java
@@ -0,0 +1,42 @@
+package Facebook;
+
+/**
+ * Given a non-empty string s, you may delete at most one character. Judge whether you can make it a palindrome.
+ * <p>
+ * Example 1:
+ * Input: "aba"
+ * Output: True
+ * Example 2:
+ * Input: "abca"
+ * Output: True
+ * Explanation: You could delete the character 'c'.
+ * Note:
+ * The string will only contain lowercase characters a-z. The maximum length of the string is 50000.
+ */
+public class ValidPalindrome {
+    public boolean validPalindrome(String s) {
+        int l = 0;
+        int r = s.length() - 1;
+        while (l <= r) {
+            if (s.charAt(l) == s.charAt(r)) {
+                l++;
+                r--;
+            } else {
+                return isPalindrome(s, l, r - 1) || isPalindrome(s, l + 1, r);
+            }
+        }
+        return true;
+    }
+
+    private boolean isPalindrome(String str, int s, int t) {
+        while (s <= t) {
+            if (str.charAt(s) == str.charAt(t)) {
+                s++;
+                t--;
+            } else
+                return false;
+        }
+
+        return true;
+    }
+}
