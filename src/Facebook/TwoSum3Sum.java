b525952bddc4f85cbec2294c91e6c2c7eba53531
Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>
2019-07-21 17:42:07 -0700

    Facebook problems added
    Signed-off-by: Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>

---
+++ src/Facebook/TwoSum3Sum.java
@@ -0,0 +1,48 @@
+package Facebook;
+
+import java.util.*;
+
+/**
+ * Created by Raj on 7/21/19.
+ */
+public class TwoSum3Sum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public List<List<Integer>> threeSum(int[] num, int target) {
+        Arrays.sort(num);
+        List<List<Integer>> res = new LinkedList<>();
+        for (int i = 0; i < num.length - 2; i++) {
+            if (i == 0 || (i > 0 && num[i] != num[i - 1])) {
+                int lo = i + 1, hi = num.length - 1, sum = target - num[i];
+                while (lo < hi) {
+                    if (num[lo] + num[hi] == sum) {
+                        res.add(Arrays.asList(num[i], num[lo], num[hi]));
+                        while (lo < hi && num[lo] == num[lo + 1]) lo++;
+                        while (lo < hi && num[hi] == num[hi - 1]) hi--;
+                        lo++;
+                        hi--;
+                    } else if (num[lo] + num[hi] < sum) {
+                        // improve: skip duplicates
+                        while (lo < hi && num[lo] == num[lo + 1]) lo++;
+                        lo++;
+                    } else {
+                        // improve: skip duplicates
+                        while (lo < hi && num[hi] == num[hi - 1]) hi--;
+                        hi--;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
