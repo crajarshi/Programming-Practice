c8e00ff4afe9e3cf3634cd28c1af01bd1d3ef796
Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>
2019-07-24 21:22:07 -0700

    Facebook problems added
    Signed-off-by: Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>

---
+++ src/Facebook/LCABinaryTree.java
@@ -0,0 +1,19 @@
+package Facebook;
+
+/**
+ * Created by Raj on 7/24/19.
+ */
+public class LCABinaryTree {
+    //O(N) both
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == p || root == q || root == null)
+            return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left == null)
+            return right;
+        if (right == null)
+            return left;
+        return root;
+    }
+}
