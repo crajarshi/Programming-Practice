e10202e8b5e629dccf443db456140ae3ece12d78
Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>
2019-07-28 10:40:55 -0700

    Facebook problems added
    Signed-off-by: Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>

--- src/Facebook/MeetingRooms.java
+++ src/Facebook/MeetingRooms.java
@@ -4,6 +4,8 @@ import Facebook.IntervalListIntersection.Interval;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), determine if a person could attend all meetings.
@@ -19,6 +21,23 @@ import java.util.Comparator;
  * NOTE: input types have been changed on April 15, 2019. Please reset to default code definition to get new method signature.
  */
 public class MeetingRooms {
+    public static int minMeetingRooms(int[][] intervals) {
+        Map<Integer, Integer> changes = new TreeMap<>();
+        for (int[] i : intervals) {
+            changes.put(i[0], changes.getOrDefault(i[0], 0) + 1);
+            changes.put(i[1], changes.getOrDefault(i[1], 0) - 1);
+        }
+        int room = 0, maxrooms = 0;
+        for (int v : changes.values())
+            maxrooms = Math.max(maxrooms, room += v);
+        return maxrooms;
+    }
+
+    public static void main(String[] args) {
+        int[][] intervals = {{0, 30}, {15, 20}, {5, 10},};
+        System.out.println(minMeetingRooms(intervals));
+    }
+
     public boolean canAttendMeetings(Interval[] intervals) {
         Arrays.sort(intervals, new Comparator<Interval>() {
             public int compare(Interval i1, Interval i2) {
