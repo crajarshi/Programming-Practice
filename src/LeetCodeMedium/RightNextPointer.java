2c7b9338f0349242eb42b8c9bae19dc5a17ab41e
Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>
2019-11-17 19:00:37 -0800

    Facebook problems added
    Signed-off-by: Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>

--- src/LeetCodeMedium/RightNextPointer.java
+++ src/LeetCodeMedium/RightNextPointer.java
@@ -46,6 +46,18 @@ public class RightNextPointer {
         return dummy;
     }
 
+    public Node connectDFS(Node root) {
+        dfs(root, null);
+        return root;
+    }
+
+    private void dfs(Node curr, Node next) {
+        if (curr == null) return;
+        curr.next = next;
+        dfs(curr.left, curr.right);
+        dfs(curr.right, curr.next == null ? null : curr.next.left);
+    }
+
     class Node {
         public int val;
         public Node left;
