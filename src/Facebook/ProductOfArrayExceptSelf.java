c6c73c437b2b3f5e7efe812a8e2d11d3b2e4210a
Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>
2020-03-16 23:15:58 -0700

    Facebook problems added
    Signed-off-by: Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>

--- src/Facebook/ProductOfArrayExceptSelf.java
+++ src/Facebook/ProductOfArrayExceptSelf.java
@@ -13,6 +13,24 @@ package Facebook;
  * Follow up:
  * Could you solve it with constant space complexity?
  * (The output array does not count as extra space for the purpose of space complexity analysis.)
+ *
+ *
+ * Given numbers [2, 3, 4, 5], regarding the third number 4,
+ * the product of array except 4 is 2*3*5 which consists of two parts: left 2*3 and right 5. The product is left*right. We can get lefts and rights:
+
+ Numbers:     2    3    4     5
+ Lefts:            2  2*3 2*3*4
+ Rights:  3*4*5  4*5    5
+ Let’s fill the empty with 1:
+
+ Numbers:     2    3    4     5
+ Lefts:       1    2  2*3 2*3*4
+ Rights:  3*4*5  4*5    5     1
+ We can calculate lefts and rights in 2 loops. The time complexity is O(n).
+
+ We store lefts in result array.
+ If we allocate a new array for rights. The space complexity is O(n).
+ To make it O(1), we just need to store it in a variable which is right in @lycjava3’s code.
  */
 public class ProductOfArrayExceptSelf {
     public int[] productExceptSelf(int[] nums) {
