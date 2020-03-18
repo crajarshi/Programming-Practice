d3b4c91a120efb11cc9a632ba5f8a3df91e1c05d
Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>
2020-03-17 21:24:28 -0700

    Facebook problems added
    Signed-off-by: Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>

--- src/Facebook/IntegerToEnglishWords.java
+++ src/Facebook/IntegerToEnglishWords.java
@@ -22,40 +22,34 @@ package Facebook;
  */
 public class IntegerToEnglishWords {
 
-    private static final String[] LESS_THAN_20 = {"", "One", "Two", "Three",
-            "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-    private static final String[] TENS = {"", "Ten", "Twenty", "Thirty", "Forty",
-            "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-    private static final String[] THOUSANDS = {"", "Thousand", "Million",
-            "Billion"};
+    private static final String[] belowTen = new String[]{"", "One", "Two",
+            "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+    private static final String[] belowTwenty = new String[]{"Ten", "Eleven",
+            "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    private static final String[] belowHundred = new String[]{"", "Ten", "Twenty",
+            "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
 
     public static String numberToWords(int num) {
         if (num == 0) return "Zero";
-
-        int i = 0;
-        String words = "";
-
-        while (num > 0) {
-            if (num % 1000 != 0)
-                words = helper(num % 1000) + THOUSANDS[i] + " " + words;
-            num /= 1000;
-            i++;
-        }
-
-        return words.trim();
+        return helper(num);
     }
 
     private static String helper(int num) {
-        if (num == 0)
-            return "";
-        else if (num < 20)
-            return LESS_THAN_20[num] + " ";
+        String result = new String();
+        if (num < 10) result = belowTen[num];
+        else if (num < 20) result = belowTwenty[num - 10];
         else if (num < 100)
-            return TENS[num / 10] + " " + helper(num % 10);
+            result = belowHundred[num / 10] + " " + helper(num % 10);
+        else if (num < 1000)
+            result = helper(num / 100) + " Hundred " + helper(num % 100);
+        else if (num < 1000000)
+            result = helper(num / 1000) + " Thousand " + helper(num % 1000);
+        else if (num < 1000000000)
+            result = helper(num / 1000000) + " Million " + helper(num % 1000000);
         else
-            return LESS_THAN_20[num / 100] + " Hundred " + helper(num % 100);
+            result = helper(num / 1000000000) + " Billion " + helper(num % 1000000000);
+        return result.trim();
     }
-
     public static void main(String[] args) {
 //        System.out.println(numberToWords(457020720));
         System.out.println(numberToWords(2000));
