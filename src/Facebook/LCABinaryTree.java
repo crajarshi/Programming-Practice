7ba10b5e82e2c24a32f8e0fc5feb6fa9ccee3b78
Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>
2020-03-18 21:01:11 -0700

    Facebook problems added
    Signed-off-by: Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>

--- src/Facebook/LCABinaryTree.java
+++ src/Facebook/LCABinaryTree.java
@@ -29,6 +29,16 @@ package Facebook;
  */
 public class LCABinaryTree {
     //O(N) both
+
+    /**
+     * It's recursive and expands the meaning of the function. If the current (sub)tree contains both p and q, then the function result is their LCA.
+     * If only one of them is in that subtree, then the result is that one of them. If neither are in that subtree, the result is null/None/nil.
+     *
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if (root == p || root == q || root == null)
             return root;
