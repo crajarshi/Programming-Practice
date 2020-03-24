4d243473d0578e0724d25865725df93ac6fb6b03
Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>
2020-03-23 20:51:19 -0700

    Facebook problems added
    Signed-off-by: Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>

--- src/Facebook/MergeInterval.java
+++ src/Facebook/MergeInterval.java
@@ -18,33 +18,23 @@ import java.util.*;
  * Explanation: Intervals [1,4] and [4,5] are considered overlapping.
  */
 public class MergeInterval {
-    public int[][] merge(int[][] intervals) {
-        List<int[]> res = new ArrayList<>();
-        if (intervals.length == 0 || intervals == null)
-            return res.toArray(new int[0][]);
+    /**
+     * This is also Meeting ROOM 1 problem.
+     * Given an array of meeting time intervals consisting of start and end times
+     * [[s1,e1],[s2,e2],...] (si < ei), determine if a person could attend all meetings.
+     * <p>
+     * Input: [[0,30],[5,10],[15,20]]
+     * Output: false
+     * Input: [[7,10],[2,4]]
+     * Output: true
+     **/
 
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
-
-        int start = intervals[0][0];
-        int end = intervals[0][1];
-
-        for (int[] i : intervals) {
-            if (i[0] <= end) {
-                end = Math.max(end, i[1]);
-            } else {
-                res.add(new int[]{start, end});
-                start = i[0];
-                end = i[1];
-            }
+    public static boolean canAttendMeetings(Interval[] intervals) {
+        Arrays.sort(intervals, (a, b) -> a.start - b.start);
+        for (int i = 0; i < intervals.length - 1; i++) {
+            if (intervals[i].end > intervals[i + 1].start) return false;
         }
-        //// Add the last interval
-        res.add(new int[]{start, end});
-        // toArray() method returns an array of type Object(Object[]). We need to typecast it to Integer before using as Integer objects.
-        // If we do not typecast, we get compilation error. Consider the following example:
-
-
-        return res.toArray(new int[0][]);
-
+        return true;
     }
 
 
@@ -74,13 +64,58 @@ public class MergeInterval {
 //        return result;
 //    }
 
+    public static void main(String[] args) {
+        Interval interval1 = new Interval(0, 30);
+        Interval interval2 = new Interval(5, 10);
+        Interval interval3 = new Interval(15, 20);
+
+        Interval[] intervals = new Interval[3];
+        intervals[0] = interval1;
+        intervals[1] = interval2;
+        intervals[2] = interval3;
+
+        System.out.println(canAttendMeetings(intervals));
+    }
+
+    public int[][] merge(int[][] intervals) {
+        List<int[]> res = new ArrayList<>();
+        if (intervals.length == 0 || intervals == null)
+            return res.toArray(new int[0][]);
+        //Sort by ascending starting point using an anonymous Comparator
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+
+        for (int[] i : intervals) {
+            if (i[0] <= end) {
+                end = Math.max(end, i[1]);
+            } else {
+                res.add(new int[]{start, end});
+                start = i[0];
+                end = i[1];
+            }
+        }
+        //// Add the last interval
+        res.add(new int[]{start, end});
+        // toArray() method returns an array of type Object(Object[]). We need to typecast it to Integer before using as Integer objects.
+        // If we do not typecast, we get compilation error. Consider the following example:
+
+
+        return res.toArray(new int[0][]);
+
+    }
+
     /**
      * Given a collection of intervals, merge all overlapping intervals.
      * Input: [[1,3],[2,6],[8,10],[15,18]]
      * Output: [[1,6],[8,10],[15,18]]
      * Explanation:
      * <p>
-     * The idea is to sort the intervals by their starting points. Then, we take the first interval and compare its end with the next intervals starts. As long as they overlap, we update the end to be the max end of the overlapping intervals. Once we find a non overlapping interval, we can add the previous "extended" interval and start over.
+     * The idea is to sort the intervals by their starting points.
+     * Then, we take the first interval and compare its end with the next intervals starts.
+     * As long as they overlap, we update the end to be the max end of the overlapping intervals.
+     * Once we find a non overlapping interval, we can add the previous "extended" interval and start over.
      * <p>
      * Sorting takes O(n log(n)) and merging the intervals takes O(n). So, the resulting algorithm takes O(n log(n)).
      * <p>
@@ -143,25 +178,6 @@ public class MergeInterval {
     }
 
     /**
-     * This is also Meeting ROOM 1 problem.
-     * Given an array of meeting time intervals consisting of start and end times
-     * [[s1,e1],[s2,e2],...] (si < ei), determine if a person could attend all meetings.
-     * <p>
-     * Input: [[0,30],[5,10],[15,20]]
-     * Output: false
-     * Input: [[7,10],[2,4]]
-     * Output: true
-     **/
-
-    public boolean canAttendMeetings(Interval[] intervals) {
-        Arrays.sort(intervals, (a, b) -> a.start - b.start);
-        for (int i = 0; i < intervals.length - 1; i++) {
-            if (intervals[i].end > intervals[i + 1].start) return false;
-        }
-        return true;
-    }
-
-    /**
      * This is also Meeting Room 2 problem.
      * Given an array of meeting time intervals consisting of
      * start and end times [[s1,e1],[s2,e2],...] (si < ei),
@@ -215,7 +231,7 @@ public class MergeInterval {
     }
 
     //Definition for an interval.
-    public class Interval {
+    public static class Interval {
         int start;
         int end;
 
