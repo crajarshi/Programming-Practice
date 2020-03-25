5ea61ee812356e692e477aacdf0b6942a04984b3
Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>
2020-03-24 20:12:26 -0700

    Facebook problems added
    Signed-off-by: Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>

--- src/Facebook/Strobogrammatic.java
+++ src/Facebook/Strobogrammatic.java
@@ -45,4 +45,24 @@ public class Strobogrammatic {
     public static void main(String[] args) {
         System.out.println(findStrobogrammatic(3));
     }
+
+    public List<String> findStrobogrammatic2(int n) {
+        List<String> cur, ans;
+        ans = new ArrayList<String>((n & 1) == 0 ? Arrays.asList("") : Arrays.asList("0", "1", "8"));
+        if (n < 2) return ans;
+
+        for (; n > 1; n -= 2) {
+            cur = ans;
+            ans = new ArrayList<String>();
+            for (String i : cur) {
+                if (n > 3) ans.add('0' + i + '0');
+                ans.add('1' + i + '1');
+                ans.add('8' + i + '8');
+                ans.add('6' + i + '9');
+                ans.add('9' + i + '6');
+            }
+        }
+
+        return ans;
+    }
 }
