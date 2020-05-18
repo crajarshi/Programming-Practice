53e1003d66c1796269a99c7aec1b320543c4cca9
Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>
2020-05-17 23:34:52 -0700

    Facebook problems added
    Signed-off-by: Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>

---
+++ src/Google/ArraysAndStrings/TwoSum3Sum.java
@@ -0,0 +1,63 @@
+package Google.ArraysAndStrings;
+
+import java.util.*;
+
+/**
+ * Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0?
+ * Find all unique triplets in the array which gives the sum of zero.
+ * <p>
+ * Note:
+ * <p>
+ * The solution set must not contain duplicate triplets.
+ * <p>
+ * Example:
+ * <p>
+ * Given array nums = [-1, 0, 1, 2, -1, -4],
+ * <p>
+ * A solution set is:
+ * [
+ * [-1, 0, 1],
+ * [-1, -1, 2]
+ * ]
+ */
+public class TwoSum3Sum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public List<List<Integer>> threeSum(int[] num, int target) {
+        Arrays.sort(num);
+        List<List<Integer>> res = new LinkedList<>();
+        for (int i = 0; i < num.length - 2; i++) {
+            if (i == 0 || (i > 0 && num[i] != num[i - 1])) {
+                int lo = i + 1, hi = num.length - 1, sum = target - num[i];
+                while (lo < hi) {
+                    if (num[lo] + num[hi] == sum) {
+                        res.add(Arrays.asList(num[i], num[lo], num[hi]));
+//                        while (lo < hi && num[lo] == num[lo + 1]) lo++;
+//                        while (lo < hi && num[hi] == num[hi - 1]) hi--;
+                        lo++;
+                        hi--;
+                    } else if (num[lo] + num[hi] < sum) {
+                        // improve: skip duplicates
+//                        while (lo < hi && num[lo] == num[lo + 1]) lo++;
+                        lo++;
+                    } else {
+                        // improve: skip duplicates
+//                        while (lo < hi && num[hi] == num[hi - 1]) hi--;
+                        hi--;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
--- src/Google/Design/LoggerRateLimitter.java
+++ src/Google/Design/LoggerRateLimitter.java
@@ -1,4 +1,4 @@
-package Google;
+package Google.Design;
 
 import java.util.HashMap;
 import java.util.Map;
