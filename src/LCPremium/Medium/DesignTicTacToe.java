397fc9d4b9d6452ede824893f1d8ba7573390458
Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>
2020-07-22 21:39:42 -0700

    Facebook problems added
    Signed-off-by: Rajarshi Chakraborty <chakraborty.rajarshi@gmail.com>

--- src/LCPremium/Medium/DesignTicTacToe.java
+++ src/LCPremium/Medium/DesignTicTacToe.java
@@ -6,9 +6,11 @@ package LCPremium.Medium;
  * <p>
  * You may assume the following rules:
  * <p>
- * A move is guaranteed to be valid and is placed on an empty block. Once a
- * winning condition is reached, no more moves is allowed. A player who succeeds
- * in placing n of their marks in a horizontal, vertical, or diagonal row wins
+ * 1.A move is guaranteed to be valid and is placed on an empty block. Once a
+ * 2.winning condition is reached, no more moves is allowed.
+ * 3. A player who succeeds in placing n of their marks in a horizontal
+ * vertical, or
+ * diagonal row wins
  * the game.
  * <p>
  * Given n = 3, assume that player 1 is "X" and player 2 is "O" in the board.
