4c0cf03c6240ee1ac4c4e7dd6816a59a10634e28
Raj Chakraborty <chrajars@amazon.com>
2023-10-03 10:09:16 -0700

    3sum

--- src/Facebook/TwoSum3Sum.java
+++ src/Facebook/TwoSum3Sum.java
@@ -37,7 +37,7 @@ public class TwoSum3Sum {
         Arrays.sort(num);
         List<List<Integer>> res = new LinkedList<>();
         for (int i = 0; i < num.length - 2; i++) {
-            if (i == 0 || (i > 0 && num[i] != num[i - 1])) {
+            if (i == 0 || (i > 0 && num[i] != num[i - 1])) {//Skip diplicates
                 int lo = i + 1, hi = num.length - 1, sum = target - num[i];
                 while (lo < hi) {
                     if (num[lo] + num[hi] == sum) {
