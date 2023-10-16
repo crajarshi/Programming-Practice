f666bbf43bbd038a80b7b04ca600a8b6a33f8986
Raj Chakraborty <chrajars@amazon.com>
2023-10-15 21:08:46 -0700

    FB 2023

--- src/Meta2023LCPremium/RemoveDuplicatesFromSortedArray.java
+++ src/Meta2023LCPremium/RemoveDuplicatesFromSortedArray.java
@@ -37,6 +37,19 @@ package Meta2023LCPremium;
  * }
  */
 public class RemoveDuplicatesFromSortedArray {
+
+    public static int removeDuplicatesBetter(int[] nums)
+    {
+        int j = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+        if (nums[i] != nums[i - 1]) {
+            nums[j] = nums[i];
+            j++;
+        }
+    }
+        return j;
+    }
     public static int removeDuplicates(int[] nums) {
         int i = nums.length > 0 ? 1 : 0;
         for (int n : nums)
